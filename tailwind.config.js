0d4fb551e30377f09aca22cbf5d158ea3b225132
Raul Escamilla <raul.escamilla@asesoriait.com>
2023-05-08 15:24:25 -0600

    Border-radius styles in themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,13 @@ module.exports = {
       borderRadius: {
         '4xl': '2rem',
       }
-    }
+    },
+    borderRadius: {
+      'none': '0',
+      'sm': '.125rem',
+      DEFAULT: '.25rem',
+      'lg': '.5rem',
+      'full': '9999px',
+    },
   }
 }
