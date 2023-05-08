55447fd0346acf6678c60e305000cc4ccf9894f4
Raul Escamilla <raul.escamilla@asesoriait.com>
2023-05-08 17:01:57 -0600

    Colors on theme config changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,18 +12,60 @@ module.exports = {
     colors: {
       'primary': {
         main: '#2f3542',
-        100: '#f8f9fa',
-        200: '#e9ecef',
-        300: '#dee2e6',
+        light: '#f8f9fa',
+        dark: '#e9ecef',
+        hover: '#dee2e6'
       },
-      'secondary': '#5352ed',
-      'danger': '#ff4757',
-      'success': '#20bf6b',
-      'warning': '#ff6348',
-      'info': '#34e7e4',      
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
+      'secondary': {
+        main: '#f1f2f6',
+        light: '#f8f9fa',
+        dark: '#e9ecef',
+        hover: '#dee2e6',
+      },
+      'success': {
+        main: '#2ecc71',
+        light: '#f8f9fa',
+        dark: '#e9ecef',
+        hover: '#dee2e6',
+      },
+      'error': {
+        main: '#e74c3c',
+        light: '#f8f9fa',
+        dark: '#e9ecef',
+        hover: '#dee2e6',
+      },
+      'warning': {
+        main: '#f1c40f',
+        light: '#f8f9fa',
+        dark: '#e9ecef',
+        hover: '#dee2e6',
+      },
+      'info': {
+        main: '#3498db',
+        light: '#f8f9fa',
+        dark: '#e9ecef',
+        hover: '#dee2e6',
+      },
+      'success': {
+        main: '#2ecc71',
+        light: '#f8f9fa',
+        dark: '#e9ecef',
+        hover: '#dee2e6',
+      },
+      'error': {
+        main: '#e74c3c',
+        light: '#f8f9fa',
+        dark: '#e9ecef',
+        hover: '#dee2e6',
+      },
+      'gray': {
+        main: '#4b6584',
+        100: '#95afc0',
+        200: '#a5b1c2',
+        300: '#b2bec3',
+        400: '#dfe4ea',
+        500: '#f1f2f6',
+      },
     },
     fontFamily: {
       'raleway': ['Raleway', 'sans-serif']
